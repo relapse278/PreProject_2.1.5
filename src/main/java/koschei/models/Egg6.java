033cd5f094a432bc0c283c94c56746a68d02f086
Ruslan <r.schewtschenko@mail.ru>
2023-10-12 11:26:16 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Egg6.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Egg6 {
+    @Autowired
+    Needle7 needle;
+
+    @Override
+    public String toString() {
+        return "в яйце — игла, " + needle.toString();
+    }
+}
